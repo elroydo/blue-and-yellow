7892c43a5e6d3d3d5ce6cacac8ceccad164d20fd
elroyD <72175303+elroydo@users.noreply.github.com>
2023-01-15 14:36:24 +0000

    feat (time): nothing changes
    and yet, everything changes

---
+++ basics/variables.py
@@ -0,0 +1,13 @@
+def main():
+    plant = 'Pot!'
+    print(plant)
+    
+    plant = 'Snoop?'
+    print(plant)
+    
+    snoop_dogg = 'God save the Queen.'
+    print(snoop_dogg)
+    
+main()
+
+#very varying variables...
