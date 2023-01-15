7dcee8c1283ed26f5b6d629f14ab350fd7778971
elroyD <72175303+elroydo@users.noreply.github.com>
2023-01-15 18:51:40 +0000

    feat (dimensions): so many possibilities!
    conditions

---
+++ basics/what-if.py
@@ -0,0 +1,12 @@
+def main():
+    you = input('0 or 1 or 3?\n') #(false and you'd be dead)
+    
+    if(int(you)==1):
+        print('Congratulations, you\'re dead!')
+    elif(int(you)==3):
+        print('...this is an anomally...')
+    else:
+        print('Oh no, you\'re alive...')
+    #possibnilities and probabilities: infinity!    
+       
+main()
