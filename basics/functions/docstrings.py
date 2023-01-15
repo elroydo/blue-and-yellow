2f7217f4b2ba154daf8b8f13fccd28bb60d82337
elroyD <72175303+elroydo@users.noreply.github.com>
2023-01-15 21:48:40 +0000

    feat (help): docs with string attached
    help for help...

---
+++ basics/functions/docstrings.py
@@ -0,0 +1,17 @@
+def help():
+    """
+    Provides help for the help function.
+    I know, incredibly useful!
+    What could help help about?
+    What does help even mean?!
+    It's a mystery to all but dolphins...
+    Only if they could communicate with humans.
+    """
+    
+def main():
+    help()
+    
+main()
+
+help.__doc__
+#no idea how to make use of this like this in a file...
