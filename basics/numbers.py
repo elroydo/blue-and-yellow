92b2ec7c196b9658ca8c22ed5bdb6cef1dd3e868
elroyD <72175303+elroydo@users.noreply.github.com>
2023-01-15 15:47:49 +0000

    feat (aleph): numbers...
    infinity

---
+++ basics/numbers.py
@@ -0,0 +1,14 @@
+def main():
+    #numbers with personality
+    print(33 + 33, 3**3, 33/3, (33*3)/3)
+    
+    #chaos
+    print(0.33+0.33, 0.33-0.22, 0.33/0.33, 0.33*3)
+    #entropy...
+    print(0.1+0.2)
+    
+    #underscores for blind people
+    stairs = 33_000_000
+    print(stairs)
+    
+main()
